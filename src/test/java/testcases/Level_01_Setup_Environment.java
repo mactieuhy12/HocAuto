c93d7b3d3a5e37b94161b0a4c19008e8ac34a8c6
mactieuhy12 <Hoanghaiyen12a12062002@gmail.com>
2024-07-10 11:32:30 +0700

    khum
    hh

--- src/test/java/testcases/Level_01_Setup_Environment.java
+++ src/test/java/testcases/Level_01_Setup_Environment.java
@@ -1,7 +1,9 @@
 package testcases;
 
 import commons.BaseTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,13 +17,25 @@ public class Level_01_Setup_Environment extends BaseTest {
 
     @AfterClass
     public void afterClass(){
-        quitBrowser(driver);
+        //quitBrowser(driver);
     }
 
     @Test
-    public void TC_01(){
+    //public void TC_01(){
         // Get(): de truyen URL -> mo ra page day
         // Get nhan vao 1 chuoi gia tri la String -> nen phai co ""
-        driver.get("");
+       //driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+       //driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']")).click();
+    //}
+    public void TC_02(){
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+        driver.findElement(By.xpath("//input[@id='txtFirstname']")).sendKeys("Hoang Hai Yen");
+        driver.findElement(By.xpath("//input[@id='txtEmail']")).sendKeys("haiaja");
+        driver.findElement(By.xpath("//input[@id='txtCEmail']")).sendKeys("haiaja");
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Hoang Hai Yen");
+        driver.findElement(By.xpath("//input[@id='txtPhone']")).sendKeys("0357207250");
+        driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']")).click();
+
+
     }
 }
