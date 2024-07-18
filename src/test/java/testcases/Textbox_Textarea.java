9a295237e3339fb1e2b8bdd5ab6f04cd0f788258
mactieuhy12 <Hoanghaiyen12a12062002@gmail.com>
2024-07-18 17:57:12 +0700

    .

--- src/test/java/testcases/Textbox_Textarea.java
+++ src/test/java/testcases/Textbox_Textarea.java
@@ -4,7 +4,6 @@ import commons.BaseTest;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -24,11 +23,11 @@ public class Textbox_Textarea extends BaseTest {
 
     @BeforeClass
     public void beforeClass(){
-        driver = getBrowserDriver("chorme");
+        driver = getBrowserDriver("chrome");
     }
     @AfterClass
     public  void afterClass(){
-        quitBrowser(driver);
+       // quitBrowser(driver);
     }
     @Test
     public void TC_1(){
@@ -36,18 +35,22 @@ public class Textbox_Textarea extends BaseTest {
         By parentLocator = By.xpath("//span[@class = 'label' and text()= \"Account\"]");
         By childLocator = By.xpath("//div[@id = \"header-account\"]/div/ul/li");
 
+        selectItemInCustomDropDown(parentLocator, childLocator, "My Account");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Hoanghaiyen12a12062002@gmail.com");
+        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("12345678");
+        driver.findElement(By.xpath("//button[@id='send2']")).click();
 
     }
-   protected void selectItemInCustomDropDown(String parentLocator, String childLocator, String expectedItem){
+   protected void selectItemInCustomDropDown(By parentLocator, By childLocator, String expectedItem){
         //1- Click vào thẻ (cha) để xổ ra tất cả các item con
-        driver.findElement(By.xpath(parentLocator)).click();
+        driver.findElement(parentLocator).click();
 
         // 2. Chờ cho tất cả cái item con được xổ ra
         explicitWait = new WebDriverWait(driver, 20);
-        explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childLocator)));
+        explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(childLocator));
 
         //3- Đưa tất cả các item trong Dropdown vào 1 list để kiểm tra
-        List<WebElement> allItems = driver.findElements(By.xpath(childLocator));
+        List<WebElement> allItems = driver.findElements(childLocator);
          //4 - Chạy qua tất cả các giá trị đang có trong list
         for(WebElement item : allItems){
             //5 - Kiểm tra xem text của các giá trị có item nào bằng với text mong muốn không
